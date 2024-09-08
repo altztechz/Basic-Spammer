263b013dafc36777d078c348374aaa534da793b8
AltzTechz <altztechz@gmail.com>
2024-09-08 20:09:36 +0100

    Update discord.py

--- discord.py
+++ discord.py
@@ -3,6 +3,6 @@ import time
 
 time.sleep(4) # amount of time before it starts
 while True:
-    auto.write('@intoxinatedcactus11111_79157 Sussy baka?')
-    auto.press('enter')
+    auto.write('@intoxinatedcactus11111_79157 Sussy baka?') # the message you want to send don't remove the quotation marks write your message in the quotation marks.
+    auto.press('enter') # what key it needs to press after writing it since it's a spammer it'd be 'enter'
     time.sleep(1) # delay between each message being sent
