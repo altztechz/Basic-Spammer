90accacebe780e648b03fcb1c76f91a7cbc7bbfd
AltzTechz <altztechz@gmail.com>
2024-09-08 20:03:19 +0100

    Add files via upload

---
+++ discord.py
@@ -0,0 +1,8 @@
+import pyautogui as auto
+import time
+
+time.sleep(4) # amount of time before it starts
+while True:
+    auto.write('@intoxinatedcactus11111_79157 Sussy baka?')
+    auto.press('enter')
+    time.sleep(1) # delay between each message being sent
